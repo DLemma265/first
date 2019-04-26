69db8e87baef2d5d697f0f54a831b8477e7f477d
Nikita <timeless0072@gmail.com>
2019-04-26 17:02:03 +0300

    555

--- project/js/javascript.js
+++ project/js/javascript.js
@@ -38,19 +38,19 @@ let appData = {
 //     num++;
 // }
 
-// let num = 0;
-// do {
-//     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
-//         b = prompt('Во сколько обойдется?', '');
+ let num = 0;
+ do {
+    let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+        b = prompt('Во сколько обойдется?', '');
 
-//         if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-//             && a != '' && b != '' && a.length < 50) {
-//             console.log("done");
-//             appData.expenses[a] = b;        
-//         }
-//     num++;
-// }
-// while (num < 2);
+         if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+            && a != '' && b != '' && a.length < 50) {
+            console.log("done");
+             appData.expenses[a] = b;        
+         }
+     num++;
+ }
+ while (num < 2);
 
 appData.moneyPerDay = appData.budget / 30;
 
